5dde137826a4f5e77c0f522c5662dd1504b0fba0
oluwaseun <sondraycabana@gmail.com>
2021-02-02 12:08:50 +0100

    update of my platform

---
+++ web/src/test/java/com/kingcabana/KingcabanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingcabana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingcabanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
